5543c7bf991a2f6cd0370e1556e1c4281fb3ac85
Lulzat <j.lulzat@gmail.com>
2021-05-23 23:23:22 -0500

    Updated Aside

--- pages/index.vue
+++ pages/index.vue
@@ -278,7 +278,7 @@
               <label class="text-base leading-7 text-gray-500" for="grid-title">Name</label>
               <input
                 class="w-full px-4 py-2 mt-2 text-base text-black bg-blue-900 transition duration-500 ease-in-out transform border-transparent rounded-sm bg-blueGray-100 focus:border-blueGray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
-                id="grid-title" type="text" name="name" v-model="formData.name" placeholder="John Smith">
+                id="grid-title" type="text" name="name" v-model="formData.name" placeholder="eg. John Smith">
             </div>
 
             <!-- PHONE NUMBER -->
